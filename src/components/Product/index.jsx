0803a9ffa53a04257bdc978dfe68cb526f5f1c02
David Rodriguez <dalejorod@gmail.com>
2021-11-01 21:28:57 -0400

    feat(downvote): impl support for downvote

--- src/components/Product/index.jsx
+++ src/components/Product/index.jsx
@@ -2,7 +2,15 @@ import React from 'react';
 
 import './Product.css';
 
-function Product({ title, description, imageUrl, username, avatar, upvotes }) {
+function Product({
+  title,
+  description,
+  imageUrl,
+  username,
+  avatar,
+  upvotes,
+  downvotes,
+}) {
   return (
     <li className="product">
       <figure className="preview">
@@ -35,6 +43,22 @@ function Product({ title, description, imageUrl, username, avatar, upvotes }) {
               </svg>
             </button>
             <span>{upvotes}</span>
+          </div>
+          <div className="upvote product_controller">
+            <button>
+              <svg
+                width="16"
+                height="16"
+                fill="currentColor"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1z"
+                />
+              </svg>
+            </button>
+            <span>{downvotes}</span>
           </div>
         </footer>
       </div>
