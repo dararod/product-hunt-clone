266f7db84f924553dead937d7360aadb4c9a0d8d
David Rodriguez <dalejorod@gmail.com>
2021-11-09 15:18:59 -0400

    add share button

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -5,7 +5,7 @@ import './Header.css';
 function Header() {
   return (
     <header id="application-header">
-      <h1>DeveloperBreakfast</h1>
+      <h1>ProductHunt</h1>
     </header>
   );
 }
--- src/components/Product/index.jsx
+++ src/components/Product/index.jsx
@@ -16,7 +16,18 @@ function Product({
 }) {
   const handleUpvote = () => upvoteProduct(id);
   const handleDownvote = () => downvoteProduct(id);
-
+  const shareButton = async () => {
+    try {
+     await navigator.share({
+      title: title,
+      text: description,
+      url: imageUrl
+     });
+    } catch(err) {
+      console.error(err);
+    }
+  }
+  
   return (
     <li className="product">
       <figure className="preview">
@@ -65,6 +76,19 @@ function Product({
               </svg>
             </button>
             <span>{downvotes}</span>
+          </div>
+          <div className="upvote product_controller">
+            <button onClick={shareButton}>
+              <svg
+               xmlns="http://www.w3.org/2000/svg"
+               width="16"
+               height="16"
+               fill="currentColor"
+               class="bi bi-share"
+               viewBox="0 0 16 16">
+                <path d="M13.5 1a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3zM11 2.5a2.5 2.5 0 1 1 .603 1.628l-6.718 3.12a2.499 2.499 0 0 1 0 1.504l6.718 3.12a2.5 2.5 0 1 1-.488.876l-6.718-3.12a2.5 2.5 0 1 1 0-3.256l6.718-3.12A2.5 2.5 0 0 1 11 2.5zm-8.5 4a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3zm11 5.5a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3z" />
+              </svg>
+            </button>
           </div>
         </footer>
       </div>
